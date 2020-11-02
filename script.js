bebf46473f5fbfb03f463cd0d5c68855e4e8c6fb
Roman <romanhrynkoua@gmail.com>
2020-11-02 11:24:42 +0200

    appData

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let age = 24;
-let name = 'Roman';
-console.log(age, name); 
+let money = prompt("Ваш бюджет на місяць?, '');
+let time = prompt("Введіть дату в форматі YY-MM-DD", '');
+let appData = {
+  budjet: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false
+};
+let a1 = prompt("Введіть обов'язкову статтю розходів в цьому місяці", '');
+let a2 = prompt("В скільки обійдеться?", '');
+let a3 = prompt("Введіть обов'язкову статтю розходів в цьому місяці", '');
+let a4 = prompt("В скільки обійдеться?", '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
